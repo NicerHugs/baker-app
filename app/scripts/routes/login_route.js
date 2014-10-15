a48c932ab618d20aea428ad3a1101a81fc6221aa
Jess Scheuring <jscheuring@gmail.com>
2014-10-14 20:18:17 -0400

    fixes styles on new recipe page

--- app/scripts/routes/login_route.js
+++ app/scripts/routes/login_route.js
@@ -4,12 +4,15 @@ Baker.LoginRoute = Ember.Route.extend({
     var loggedIn = this.controllerFor('application').get('isLoggedIn');
     var hasAccount = this.controllerFor('application').get('hasAccount');
     var accountData = this.controllerFor('createAccount').get('accountData');
+    //if the user is already logged in, bypass the login screen
     if (loggedIn){
       this.transitionTo('index');
     }
+    //if the user is not logged in but appears to have an account
+    //try to log them in
     else if (hasAccount) {
+      //look on local storage for the users account info
       if (localStorage.getItem('bakerAuth')) {
-        // get the authData from localStorage
         var authData = JSON.parse(localStorage.getItem('bakerAuth'));
         Baker.ref.authWithCustomToken(authData.token, function(error, authData) {
           if (error === null) {
@@ -21,6 +24,8 @@ Baker.LoginRoute = Ember.Route.extend({
           }
         });
       }
+      //if they don't have local storage data look on the create account
+      //controller to see if they have just made a new account
       else if (accountData) {
         Baker.ref.authWithPassword({
           email: accountData.email,
