1c293a1cbf1ca3153fee901927da00200e742606
Jess Scheuring <jscheuring@gmail.com>
2014-10-21 23:53:24 -0400

    hides edit button for non-authors

--- app/scripts/controllers/recipes/recipe_controller.js
+++ app/scripts/controllers/recipes/recipe_controller.js
@@ -1,4 +1,13 @@
 Baker.RecipesRecipeController = Ember.ObjectController.extend({
+  needs: 'application',
+  currentUser: Ember.computed.alias('controllers.application.currentUser'),
+  isAuthor: function(){
+    if (this.get('currentUser.id') === this.get('author.id')) {
+      return true;
+    }
+    else
+      return false;
+  }.property('currentUser'),
   actions: {
     editRecipe: function() {
       this.transitionToRoute("recipes.edit");
